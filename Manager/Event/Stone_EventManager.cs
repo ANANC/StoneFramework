4e9fc3fbabbc8a1f2242ed14b0e27acd285409d4
KKKK <496007818@qq.com>
2021-12-11 00:41:30 +0800

    update:更新event

---
+++ Manager/Event/Stone_EventManager.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Stone_EventManager : Stone_Manager
+{
+    public const string Name = "Stone_EventManager";
+    public override string GetName()
+    {
+        return Stone_EventManager.Name;
+    }
+
+    private class RegisterInfo
+    {
+        public object Target;
+        public Dictionary<string, List<Delegate>> EventNameToRegisterDict;        
+    }
+
+    private Dictionary<string, Stone_EventObject> m_EventObjectDict;
+    private Dictionary<object, RegisterInfo> m_RegisterInfoDict;
+
+    public override void Init()
+    {
+        m_EventObjectDict = new Dictionary<string, Stone_EventObject>();
+        m_RegisterInfoDict = new Dictionary<object, RegisterInfo>();
+    }
+
+    public override void UnInit()
+    {
+    }
+
+    /// <summary>
+    /// 添加监听
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="target"></param>
+    /// <param name="listener"></param>
+    public void AddListener(string name,object target, Action listener)
+    {
+        Stone_EventObject eventObject;
+        if (!m_EventObjectDict.TryGetValue(name, out eventObject))
+        {
+            eventObject = new Stone_EventObject();
+            eventObject.Init(name);
+
+            m_EventObjectDict.Add(name, eventObject);
+        }
+
+        eventObject.AddListener(listener);
+
+        AddRegisterInfoInfo(name, target, listener);
+    }
+
+    /// <summary>
+    /// 添加监听
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="target"></param>
+    /// <param name="listener"></param>
+    public void AddListener(string name,object target, Action<Stone_EventObject.EventCallbackInfo> listener)
+    {
+        Stone_EventObject eventObject;
+        if (!m_EventObjectDict.TryGetValue(name, out eventObject))
+        {
+            eventObject = new Stone_EventObject();
+            eventObject.Init(name);
+
+            m_EventObjectDict.Add(name, eventObject);
+        }
+
+        eventObject.AddListener(listener);
+
+        AddRegisterInfoInfo(name, target, listener);
+    }
+
+    private void AddRegisterInfoInfo(string name, object target, Delegate listener)
+    {
+        RegisterInfo registerInfo;
+        if (!m_RegisterInfoDict.TryGetValue(target, out registerInfo))
+        {
+            registerInfo = new RegisterInfo();
+            registerInfo.Target = target;
+            registerInfo.EventNameToRegisterDict = new Dictionary<string, List<Delegate>>();
+
+            m_RegisterInfoDict.Add(target, registerInfo);
+        }
+        List<Delegate> delegateList;
+        if (!registerInfo.EventNameToRegisterDict.TryGetValue(name, out delegateList))
+        {
+            delegateList = new List<Delegate>();
+            registerInfo.EventNameToRegisterDict.Add(name, delegateList);
+        }
+        delegateList.Add(listener);
+    }
+
+    /// <summary>
+    /// 删除监听
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="target"></param>
+    /// <param name="listener"></param>
+    public void DeleteListener(string name, object target, Delegate listener)
+    {
+        Stone_EventObject eventObject;
+        if (!m_EventObjectDict.TryGetValue(name, out eventObject))
+        {
+            return;
+        }
+
+        eventObject.DeleteListern(listener);
+
+        RegisterInfo registerInfo;
+        if (m_RegisterInfoDict.TryGetValue(target, out registerInfo))
+        {
+            List<Delegate> delegateList;
+            if (registerInfo.EventNameToRegisterDict.TryGetValue(name, out delegateList))
+            {
+                delegateList.Remove(listener);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 删除目标的全部监听
+    /// </summary>
+    /// <param name="target"></param>
+    public void DeleteTargetAllListener(object target)
+    {
+        RegisterInfo registerInfo;
+        if (m_RegisterInfoDict.TryGetValue(target, out registerInfo))
+        {
+            Dictionary<string, List<Delegate>>.Enumerator enumerator = registerInfo.EventNameToRegisterDict.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                string eventName = enumerator.Current.Key;
+
+                Stone_EventObject eventObject;
+                if (!m_EventObjectDict.TryGetValue(eventName, out eventObject))
+                {
+                    continue;
+                }
+
+                List<Delegate> listenerList = enumerator.Current.Value;
+
+                for (int index = 0;index<listenerList.Count;index++)
+                {
+                    Delegate listener = listenerList[index];
+                    eventObject.DeleteListern(listener);
+                }
+            }
+        }
+
+        m_RegisterInfoDict.Remove(target);
+    }
+
+    /// <summary>
+    /// 执行
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="info"></param>
+    public void Execute(string name, Stone_EventObject.EventCallbackInfo info = null)
+    {
+
+    }
+
+}
---
+++ Manager/Event/Stone_EventObject.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone_EventObject 
+{
+    public class EventCallbackInfo { }
+
+    private string m_Name;
+
+    private Dictionary<Delegate, int> m_RegisterSortDict;
+
+    private List<Delegate> m_AddRegisterList;
+    private List<Delegate> m_DeleteList;
+
+    private Dictionary<Delegate, int> m_AllRegisterDict;
+    private List<Delegate> m_AllRegisterList;
+
+    private bool m_IsDirty;
+
+    public void Init(string name)
+    {
+        m_Name = name;
+
+        m_RegisterSortDict = new Dictionary<Delegate, int>();
+
+        m_AddRegisterList = new List<Delegate>();
+        m_DeleteList = new List<Delegate>();
+
+        m_AllRegisterDict = new Dictionary<Delegate, int>();
+        m_AllRegisterList = new List<Delegate>();
+
+        m_IsDirty = false;
+    }
+
+    public void UnInit()
+    {
+        m_RegisterSortDict.Clear();
+        m_AddRegisterList.Clear();
+        m_DeleteList.Clear();
+        m_AllRegisterDict.Clear();
+        m_AllRegisterList.Clear();
+    }
+
+    /// <summary>
+    /// 添加监听者
+    /// </summary>
+    /// <param name="listener"></param>
+    /// <param name="sort"></param>
+    public void AddListener(Action listener,int sort = -1)
+    {
+        AddListener(listener, sort);
+    }
+
+    /// <summary>
+    /// 添加监听者
+    /// </summary>
+    /// <param name="listener"></param>
+    /// <param name="sort"></param>
+    public void AddListener(Action<EventCallbackInfo> listener, int sort = -1)
+    {
+        AddListener(listener, sort);
+    }
+
+    private void AddListener(Delegate listener, int sort)
+    {
+        if (m_AllRegisterDict.ContainsKey(listener))
+        {
+            return;
+        }
+
+        m_IsDirty = true;
+
+        if (sort != -1)
+        {
+            bool hasNewSort = false;
+            Dictionary<Delegate, int>.Enumerator enumerator = m_RegisterSortDict.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                int delegateSort = enumerator.Current.Value;
+
+                if (delegateSort == sort)
+                {
+                    hasNewSort = true;
+                    break;
+                }
+            }
+            if (hasNewSort)
+            {
+                for (int index = 0; index < m_AllRegisterList.Count; index++)
+                {
+                    Delegate register = m_AllRegisterList[index];
+                    int delegateSort = m_RegisterSortDict[register];
+                    m_RegisterSortDict[register] = delegateSort + 1;
+                }
+                m_RegisterSortDict.Add(listener, sort);
+            }
+        }
+
+        m_AddRegisterList.Add(listener);
+    }
+
+
+    /// <summary>
+    /// 删除监听者
+    /// </summary>
+    /// <param name="listener"></param>
+    public void DeleteListern(Delegate listener)
+    {
+        m_IsDirty = true;
+
+        m_DeleteList.Add(listener);
+    }
+
+    /// <summary>
+    /// 执行
+    /// </summary>
+    /// <param name="info"></param>
+    public void Execute(EventCallbackInfo info = null)
+    {
+        TryRefresh();
+
+        for (int index = 0; index < m_AllRegisterList.Count; index++)
+        {
+            Delegate listener = m_AllRegisterList[index];
+
+            if (listener is Action)
+            {
+                Action action = (Action)listener;
+                try
+                {
+                    action();
+                }
+                catch (Exception exception)
+                {
+                    LogHelper.Error?.Log("Event", m_Name, "Execute Error.", exception.Message);
+                }
+            }
+            if (listener is Action<EventCallbackInfo>)
+            {
+                Action<EventCallbackInfo> paramAction = (Action<EventCallbackInfo>)listener;
+                try
+                {
+                    paramAction(info);
+                }
+                catch (Exception exception)
+                {
+                    LogHelper.Error?.Log("Event", m_Name, "Execute Error.", exception.Message);
+                }
+            }
+        }
+
+        TryRefresh();
+    }
+
+    private void TryRefresh()
+    {
+        if (m_IsDirty == false)
+        {
+            return;
+        }
+
+        m_IsDirty = false;
+
+        for (int index = 0; index < m_AddRegisterList.Count; index++)
+        {
+            Delegate listener = m_AddRegisterList[index];
+
+            if (m_AllRegisterDict.ContainsKey(listener))
+            {
+                continue;
+            }
+
+            int sort;
+            if (!m_RegisterSortDict.TryGetValue(listener, out sort))
+            {
+                sort = -1;
+            }
+
+            m_AllRegisterDict.Add(listener, sort);
+            m_AllRegisterList.Add(listener);
+        }
+        m_AddRegisterList.Clear();
+
+        for (int index = 0; index < m_DeleteList.Count; index++)
+        {
+            Delegate listener = m_DeleteList[index];
+
+            if (!m_AllRegisterDict.ContainsKey(listener))
+            {
+                continue;
+            }
+
+            m_AllRegisterDict.Remove(listener);
+            m_AllRegisterList.Remove(listener);
+
+            m_RegisterSortDict.Remove(listener);
+        }
+        m_DeleteList.Clear();
+
+        m_AllRegisterList.Sort(RegisterListSort);
+    }
+
+    private int RegisterListSort(Delegate left, Delegate right)
+    {
+        int leftSort = m_AllRegisterDict[left];
+        int rightSort = m_AllRegisterDict[right];
+
+        return leftSort.CompareTo(rightSort);
+    }
+}
